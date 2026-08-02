092542679a71a1c1797db8ba5e0c72b23af81331
gepardoj <anna.ra.phoenix@gmail.com>
2026-08-02 21:56:55 +0200

    init

---
+++ Assets/Scripts/MazeGenerator.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class MazeGenerator : MonoBehaviour
+{
+    void Start()
+    {
+        GenerateMaze();
+        SpawnMazeObjects();
+    }
+
+    private void GenerateMaze()
+    {
+
+    }
+
+    private void SpawnMazeObjects()
+    {
+
+    }
+}
